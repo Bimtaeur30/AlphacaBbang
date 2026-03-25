5796284da447057e26fd2890680931f5e2c83f4d
ForRest1217 <Jihan2519@naver.com>
2026-03-25 18:24:41 +0900

    ObjectPool&SoundManager

---
+++ AlphacaBbang/Assets/MemberWorkspace/JJH/02_Scripts/Systems/ObjectPoolSystems/PoolableMono.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace JJH._02_Scripts.Systems.ObjectPoolSystems
+{
+    public abstract class PoolableMono : MonoBehaviour, IPoolable
+    {
+        [field: SerializeField] public PoolItemSO PoolType { get; set; }
+        public GameObject GameObject => this != null ? gameObject : null;
+        public virtual void ResetItem() { }
+    }
+}

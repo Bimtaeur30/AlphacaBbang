f424fed8a644031e7064298505488f6527ca98b8
ironyo <kyou09kang@gmail.com>
2026-04-01 19:32:26 +0900

    PlayerMovement
    조준할때 마우스보고 움직이는건 끝났고, 이제 조준할때 게이지 소모해야함

--- AlphacaBbang/Assets/MemberWorkspace/KU/02_Scripts/Player/PlayerInputHandler.cs
+++ AlphacaBbang/Assets/MemberWorkspace/KU/02_Scripts/Player/PlayerInputHandler.cs
@@ -5,5 +5,21 @@ public class PlayerInputHandler : MonoBehaviour
 {
     [SerializeField] private PlayerInputSO playerInput;
 
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        playerInput.SetMovement(context.ReadValue<Vector2>());
+    }
 
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        playerInput.SetLook(context.ReadValue<Vector2>());
+    }
+
+    public void OnAim(InputAction.CallbackContext context)
+    {
+        if (context.started)
+            playerInput.SetAim(true);
+        else if (context.canceled)
+            playerInput.SetAim(false);
+    }
 }

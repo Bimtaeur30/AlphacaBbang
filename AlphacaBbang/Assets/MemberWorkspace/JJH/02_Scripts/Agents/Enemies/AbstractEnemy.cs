b5ec37fcb75000b157830239a73a02fe4c8857b4
ForRest1217 <Jihan2519@naver.com>
2026-04-01 19:13:14 +0900

    EnemyMove

--- AlphacaBbang/Assets/MemberWorkspace/JJH/02_Scripts/Agents/Enemies/AbstractEnemy.cs
+++ AlphacaBbang/Assets/MemberWorkspace/JJH/02_Scripts/Agents/Enemies/AbstractEnemy.cs
@@ -1,13 +1,10 @@
 using Unity.Behavior;
-using UnityEngine;
 
 namespace JJH._02_Scripts.Agents.Enemies
 {
-    public abstract class AbstractEnemy : MonoBehaviour
+    public abstract class AbstractEnemy : Agent
     {
         public BehaviorGraphAgent BTAgent { get; private set; }
-        public AgentSensor Sensor { get; private set; }
-
         public AttackDataSO AttackData { get; private set; }
     }
 }
--- AlphacaBbang/Assets/MemberWorkspace/KU/02_Scripts/Agent/Agent.cs
+++ AlphacaBbang/Assets/MemberWorkspace/KU/02_Scripts/Agent/Agent.cs
@@ -1,6 +1,16 @@
-using UnityEngine;
+using JJH._02_Scripts.Agents;
 
 public class Agent : ModuleOwner
 {
+    public AgentSensor Sensor { get; private set; }
+    public IRenderer Renderer { get; private set; }
+    public IControllerMovement Movement { get; private set; }
 
+    protected override void InitializeComponents()
+    {
+        base.InitializeComponents();
+        Sensor = GetModule<AgentSensor>();
+        Renderer = GetModule<IRenderer>();
+        Movement = GetModule<IControllerMovement>();
+    }
 }

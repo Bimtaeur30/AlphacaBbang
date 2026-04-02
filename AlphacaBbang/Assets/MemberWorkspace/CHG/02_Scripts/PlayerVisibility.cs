64b37ac34dc3c3e7b4760e46be7af5dff955fe19
chohangyeol09 <chohangyeol09@gmail.com>
2026-04-02 17:51:03 +0900

    Feat : PlayerVisiblity

---
+++ AlphacaBbang/Assets/MemberWorkspace/CHG/02_Scripts/PlayerVisibility.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace MemberWorkspace.CHG._02_Scripts
+{
+    public class PlayerVisibility : MonoBehaviour
+    {
+        private void Update()
+        {
+            RotateToMouse(); //test
+        }
+
+        #region TestCodes
+
+        private void RotateToMouse()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+            if (Physics.Raycast(ray, out RaycastHit hit, 100f))
+            {
+                Vector3 lookPoint = hit.point;
+                lookPoint.y = transform.position.y;
+
+                Vector3 dir = (lookPoint - transform.position).normalized;
+
+                if (dir.sqrMagnitude < 0.001f)
+                    return;
+
+                Quaternion targetRotation = Quaternion.LookRotation(dir);
+
+                transform.rotation = Quaternion.Lerp(
+                    transform.rotation,
+                    targetRotation,
+                    10f * Time.deltaTime);
+            }
+        }
+
+        #endregion
+
+    }
+}

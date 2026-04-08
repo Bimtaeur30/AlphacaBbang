1893873c058060836bb9302b263cf4045758c7d7
ForRest1217 <Jihan2519@naver.com>
2026-04-08 19:24:40 +0900

    EnemyChase

--- AlphacaBbang/Assets/MemberWorkspace/JJH/02_Scripts/Agents/Enemies/AbstractEnemy.cs
+++ AlphacaBbang/Assets/MemberWorkspace/JJH/02_Scripts/Agents/Enemies/AbstractEnemy.cs
@@ -1,3 +1,4 @@
+using MemberWorkspace.JJH._02_Scripts.Agents.Enemies.NavMesh;
 using UnityEngine;
 
 namespace JJH._02_Scripts.Agents.Enemies
@@ -5,5 +6,12 @@ namespace JJH._02_Scripts.Agents.Enemies
     public abstract class AbstractEnemy : Agent
     {
         [field: SerializeField] public AttackDataSO AttackData { get; private set; }
+        public INavMeshAgent NavMeshAgent { get; private set; }
+
+        protected override void InitializeComponents()
+        {
+            base.InitializeComponents();
+            NavMeshAgent = GetModule<INavMeshAgent>();
+        }
     }
 }
---
+++ AlphacaBbang/Assets/MemberWorkspace/JJH/02_Scripts/Agents/Enemies/NavMesh/EnemyNavMeshAgent.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace MemberWorkspace.JJH._02_Scripts.Agents.Enemies.NavMesh
+{
+    public class EnemyNavMeshAgent : MonoBehaviour, IModule, INavMeshAgent
+    {
+        private ModuleOwner _owner;
+        private NavMeshAgent _navMeshAgent;
+
+        public void Initialize(ModuleOwner owner)
+        {
+            _owner = owner;
+            _navMeshAgent = GetComponentInParent<NavMeshAgent>();
+        }
+
+        public void MoveTo(Vector3 targetPosition)
+        {
+            _navMeshAgent.SetDestination(targetPosition);
+        }
+
+        public void KeepChase(bool value)
+        {
+            _navMeshAgent.isStopped = !value;
+        }
+    }
+}

e0f00882be23580d66415955f326ccdcc0f3b041
Bimtaeur30 <Bimtaeur30@gmail.com>
2026-04-12 15:50:17 +0900

    GunCameraShake

--- AlphacaBbang/Assets/MemberWorkspace/KTJ/02_Scripts/Weapon/Gun/GunDataSO.cs
+++ AlphacaBbang/Assets/MemberWorkspace/KTJ/02_Scripts/Weapon/Gun/GunDataSO.cs
@@ -34,10 +34,10 @@ public class GunDataSO : ScriptableObject
     [field: SerializeField, Range(0f, 100f)] public float RecoilForceY { get; private set; } = 1f; // 반동
 
     [Header("Accuracy")]
-    [field: SerializeField, Range(0f, 100f)] public float SpreadAngle { get; private set; } = 1f; // 탄 퍼짐(샷건전용)
+    [field: SerializeField, Range(1f, 100f)] public float SpreadAngle { get; private set; } = 1f; // 탄 퍼짐(샷건전용)
 
     [Header("Accuracy")]
-    [field: SerializeField, Range(0f, 10f)] public int BulletFireCount { get; private set; } = 1; // 한번에 나가는 총 개수(샷건전용)
+    [field: SerializeField, Range(1f, 10f)] public int BulletFireCount { get; private set; } = 1; // 한번에 나가는 총 개수(샷건전용)
 
     [Header("UI")]
     [field: SerializeField] public Sprite CrossHairSprite { get; private set; } // 조준 이미지

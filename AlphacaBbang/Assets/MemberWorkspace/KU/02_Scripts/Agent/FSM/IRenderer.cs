f08c91a8a6184e38cc18c92443cbe1eb5de2f151
ForRest1217 <Jihan2519@naver.com>
2026-04-15 23:52:06 +0900

    EnemyComplete

--- AlphacaBbang/Assets/MemberWorkspace/KU/02_Scripts/Agent/FSM/IRenderer.cs
+++ AlphacaBbang/Assets/MemberWorkspace/KU/02_Scripts/Agent/FSM/IRenderer.cs
@@ -3,6 +3,7 @@
 public interface IRenderer
 {
     Animator Animator { get; }
+    Renderer Renderer { get; }
 
     void PlayClip(int clipHash, float normalizedTime, float crossFadeDuration, int layerIndex = 0);
     public void SetFloat(int hash, float value, float dampTime = 0, float deltaTime = 0);

141b3ae946d86bcca62538b7c52179183ef20e06
Hongjihoo123 <Hongjihoo123@naver.com>
2026-04-16 18:56:21 +0900

    ShortWeapon

--- AlphacaBbang/Assets/Sword.cs
+++ AlphacaBbang/Assets/Sword.cs
@@ -1,36 +1,50 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
-public class BaseballBat : MeleeWeaponBase
+public class Sword : MeleeWeaponBase
 {
-    [SerializeField] private float comboWindow = 0.4f;
-    public int ComboCounter { get; private set; } = 0;  
+    [SerializeField] private float comboWindow = 0.75f;
+    public int ComboCounter = 0;//{ get; private set; } = 0;
+
     protected override void PerformAttack(Vector3 targetPos)
     {
-        PlayAttackParticle(targetPos);
-        
-        bool comboCounterOver = ComboCounter >= data.Length;
-        bool comboWindowExhausted = Time.time >= lastUseTime + comboWindow;
-
-        Debug.Log($"ComboCounter: {ComboCounter}");
-
-        if (comboCounterOver || comboWindowExhausted)
+        if (data[ComboCounter].attackDelay > comboWindow)
         {
-            Debug.Log("Combo reset");
-            if (data == null || data.Length == 0) return;
-            lastUseTime = Time.time;
-
-            if (ComboCounter < data.Length - 1)
-            {
-                ComboCounter++;
-            }
-            else {
-                ComboCounter = 0;
-            }
+            Debug.Assert(false, "Attack delay is bigger than lastUseTime");
         }
 
 
+        Debug.Log($"ComboCounter: {ComboCounter}");
+        Debug.Log($"comboWindow{comboWindow}, lastUseTime{lastUseTime}, ComboCounter{ComboCounter}");
+
+        bool resetCombo = Time.time > lastUseTime + comboWindow;
+
+        if (resetCombo)
+        {
+            ComboCounter = 0;
+        }
+        else
+        {
+            if (ComboCounter < data.Length - 1)
+                ComboCounter++;
+            else
+                ComboCounter = 0;
+        }
+
+        lastUseTime = Time.time;
+
         Vector3 origin = transform.position;
         Vector3 dir = (targetPos - origin).normalized;
+
+        if (ComboCounter == 2)
+        {
+            StartCoroutine(AttackDash(targetPos));
+        }
+        else
+        {
+            PlayAttackParticle(targetPos);
+        }
 
         Collider[] hits = Physics.OverlapSphere(origin, data[ComboCounter].range);
 
@@ -48,6 +62,45 @@ public class BaseballBat : MeleeWeaponBase
                 }
             }
         }
+
+        currentTime = 0;
+    }
+
+    public IEnumerator AttackDash(Vector3 targetPos)
+    {
+        Debug.Log("倒柳");
+
+        Vector3 dashDir = (targetPos - transform.parent.position).normalized;
+        dashDir.y = 0f;
+
+        float duration = 0.15f;
+        float timer = 0f;
+        float speed = 10f;
+
+        while (timer < duration)
+        {
+            transform.parent.position += dashDir * speed * Time.deltaTime;
+
+            timer += Time.deltaTime;
+
+            yield return null;
+        }
+        PlayAttackParticle(targetPos);
+    }
+
+    int count = 0;
+    public IEnumerator A()
+    {
+        if(count > 5)
+        {
+            yield break;
+        }
+        Debug.Log("内风凭 1");
+        Debug.Log("内风凭 2");
+        yield return new WaitForSeconds(1f);
+        Debug.Log("内风凭 3");
+        StartCoroutine(A());
+        count++;
     }
 
     private void OnDrawGizmosSelected()
@@ -64,13 +117,16 @@ public class BaseballBat : MeleeWeaponBase
 
         Vector3 leftDir = leftRot * forward * data[ComboCounter].range;
         Vector3 rightDir = rightRot * forward * data[ComboCounter].range;
+
         Gizmos.DrawLine(origin, origin + leftDir);
         Gizmos.DrawLine(origin, origin + rightDir);
         Gizmos.DrawWireSphere(origin, data[ComboCounter].range);
     }
+
     private void PlayAttackParticle(Vector3 targetPos)
     {
         if (data[ComboCounter].attackParticlePrefab == null) return;
+
         Vector3 origin = transform.position;
         Vector3 dir = targetPos - origin;
 
@@ -80,7 +136,8 @@ public class BaseballBat : MeleeWeaponBase
         dir.y = 0f;
 
         Quaternion rot = Quaternion.LookRotation(dir.normalized)
-                       * Quaternion.Euler(-90f, 180f, 0f);
+                       * data[ComboCounter].attackParticlePrefab.transform.rotation
+                       * Quaternion.Euler(0f, 0f, 180f);
 
         Instantiate(data[ComboCounter].attackParticlePrefab, origin, rot);
     }

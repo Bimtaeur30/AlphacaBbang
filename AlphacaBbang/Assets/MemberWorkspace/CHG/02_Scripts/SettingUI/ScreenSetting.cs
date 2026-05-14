7a56e0a813fa97b69efebc5cdcc4d5fa1f149bb3
chohangyeol09 <chohangyeol09@gmail.com>
2026-05-14 19:23:07 +0900

    Feat : AudioSetting

--- AlphacaBbang/Assets/MemberWorkspace/CHG/02_Scripts/SettingUI/ScreenSetting.cs
+++ AlphacaBbang/Assets/MemberWorkspace/CHG/02_Scripts/SettingUI/ScreenSetting.cs
@@ -1,43 +1,102 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
 
-[RequireComponent(typeof(UIDocument))]
+//[RequireComponent(typeof(UIDocument))]
 public class ScreenSetting : MonoBehaviour
 {
-    private FullScreenMode _fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+    [SerializeField] private TMP_Dropdown _resolutionDropdown;
+    [SerializeField] private TMP_Dropdown _fullScreenModeDropdown;
     private Resolution _resolution;
- 
-    private UIDocument _document;
-    private VisualElement _root;
+    private FullScreenMode _screenMode;
+    
+    //private UIDocument _document;
+    //private VisualElement _root;
     
     private void Awake()
     {
-        _document = GetComponent<UIDocument>();
-        _root = _document.rootVisualElement;
+        //_document = GetComponent<UIDocument>();
+       // _root = _document.rootVisualElement;
+        _resolutionDropdown.options.Clear();
+       foreach (Resolution resolution in GetResolutions())
+           _resolutionDropdown.options.Add(new TMP_Dropdown.OptionData(resolution.width + "x" + resolution.height));
+       
+       ResetContent();
+       StartCoroutine(SetScreenCoroutine());
+       
+       _resolutionDropdown.onValueChanged.AddListener(SetResolution);
+       _fullScreenModeDropdown.onValueChanged.AddListener(SetFullScreenMode);
+    }
+    
+    public void OnEnable()
+    {
+        List<Resolution> resolutions = GetResolutions();
+
+        int defaultIndex = resolutions.FindIndex(r => 
+            r.width == _resolution.width && 
+            r.height == _resolution.height);
+    
+        _resolutionDropdown.value = defaultIndex;
+        _fullScreenModeDropdown.value = (int)_screenMode;
+    }
+    
+    public void ResetContent()
+    {
+        _resolution = Screen.currentResolution;
+        _screenMode = FullScreenMode.ExclusiveFullScreen;
+    
+        List<Resolution> resolutions = GetResolutions();
+    
+        int defaultIndex = resolutions.FindIndex(r => 
+            r.width == Display.main.systemWidth && 
+            r.height == Display.main.systemHeight);
+    
+        _resolutionDropdown.value = defaultIndex;
+        _fullScreenModeDropdown.value = 0;
+    
+    }
+
+    private List<Resolution> GetResolutions()
+    {
+        return Screen.resolutions
+            .GroupBy(r => (r.width, r.height))
+            .Select(g => g.Last()) 
+            .OrderBy(r => r.width)
+            .ThenBy(r => r.height)
+            .ToList();
+    }
+
+    private void SetResolution(int index)
+    {
+        string[] dropDownFieldName = _resolutionDropdown.options[index].text.Split('x');
+
+        _resolution.width = int.Parse(dropDownFieldName[0]);
+        _resolution.height = int.Parse(dropDownFieldName[1]);
+    }
+
+    private void SetFullScreenMode(int index)
+    {
+        _screenMode = (FullScreenMode)index;
+    }
+
+    public void SetScreen()
+    {
+        StartCoroutine(SetScreenCoroutine());
+    }
+    
+    public IEnumerator SetScreenCoroutine()
+    {
+        Screen.SetResolution(_resolution.width, _resolution.height, _screenMode);
         
-    }
-
-    public void ChangeResolution(Resolution resolution, FullScreenMode fullScreenMode)
-    {
-        _resolution = resolution;
-        _fullScreenMode = fullScreenMode;
-        StartCoroutine(SetResolutionCoroutine());
-    }
-
-    [ContextMenu("SetResolution")]
-    private void TestSet()
-    {
-        StartCoroutine(SetResolutionCoroutine());
-    }
-
-    private IEnumerator SetResolutionCoroutine()
-    {
-        Screen.SetResolution(_resolution.width, _resolution.height, _fullScreenMode);
-
-        yield return new WaitForEndOfFrame(); 
-
+        yield return null;
+        
+        Debug.Log(_resolution.width + "x" + _resolution.height);
         int deviceWidth = Screen.width;
         int deviceHeight = Screen.height;
 
@@ -61,30 +120,35 @@ public class ScreenSetting : MonoBehaviour
         {
             cam.rect = camRect;
         }
-
-        Debug.Log("resolution: " + _resolution);
         
+
         foreach (CanvasScaler canvasScaler in FindObjectsOfType<CanvasScaler>())
         {
-            if (canvasScaler.uiScaleMode != CanvasScaler.ScaleMode.ScaleWithScreenSize)
-            {
-                canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
-                canvasScaler.referenceResolution = new Vector2(_resolution.width, _resolution.height); 
-                canvasScaler.screenMatchMode = CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;
-                canvasScaler.matchWidthOrHeight = 0.5f;
-            }
+            Debug.Log(canvasScaler.gameObject.name);
+            canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+            canvasScaler.referenceResolution = new Vector2(_resolution.width, _resolution.height);
+            canvasScaler.screenMatchMode = CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;
+            canvasScaler.matchWidthOrHeight = 0.5f;
         }
+        
     
-        if (_root != null)
+        /*if (_root != null)
         {
             _root.style.paddingLeft   = new StyleLength(camRect.x * deviceWidth);
             _root.style.paddingBottom = new StyleLength(camRect.y * deviceHeight);
             _root.style.paddingRight  = new StyleLength((1f - camRect.width - camRect.x) * deviceWidth);
             _root.style.paddingTop    = new StyleLength((1f - camRect.height - camRect.y) * deviceHeight);
-        }
-
-
+        }*/
     }
-    
-    
+
+    #region Test
+
+    [SerializeField] private GameObject parent;
+    [ContextMenu("ShowUI")]
+    private void ShowUI()
+    {
+        parent.SetActive(true);
+    }
+
+    #endregion
 }
---
+++ AlphacaBbang/Assets/MemberWorkspace/CHG/02_Scripts/SettingUI/SettingUI.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingUI : MonoBehaviour
+{
+    [SerializeField] private Dropdown resolutionDropdown;
+
+    [SerializeField] private ScreenSetting screenSetting;
+    private void Awake()
+    {
+
+    }
+}

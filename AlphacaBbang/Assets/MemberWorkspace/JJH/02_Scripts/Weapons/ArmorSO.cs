8817847c5627ed9454dccceb076ce1d08dabd09f
ForRest1217 <Jihan2519@naver.com>
2026-05-17 00:53:15 +0900

    ArmorDurability

--- AlphacaBbang/Assets/MemberWorkspace/JJH/02_Scripts/Weapons/ArmorSO.cs
+++ AlphacaBbang/Assets/MemberWorkspace/JJH/02_Scripts/Weapons/ArmorSO.cs
@@ -7,6 +7,7 @@ namespace JJH._02_Scripts.Weapons
     {
         [field: SerializeField] public int ArmorLevel { get; private set; } = 1;
         [field: SerializeField] public float DamageReductionRate { get; private set; } = 0.1f;
+        [field: SerializeField] public float ArmorDurability { get; private set; } = 100f;
         [field: SerializeField] public GameObject ArmorModel { get; private set; }
         [field: SerializeField] public ArmorType ArmorType { get; private set; }
     }

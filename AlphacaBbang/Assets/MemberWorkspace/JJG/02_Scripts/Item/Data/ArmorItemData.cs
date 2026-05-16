d7cf6854ee2508704e7e107cefb57eb2995db895
Bimtaeur30 <Bimtaeur30@gmail.com>
2026-05-17 01:55:35 +0900

    aaaaa

---
+++ AlphacaBbang/Assets/MemberWorkspace/JJG/02_Scripts/Item/Data/ArmorItemData.cs
@@ -0,0 +1,12 @@
+﻿using JJH._02_Scripts.Weapons;
+using UnityEngine;
+
+namespace MemberWorkspace.JJG._02_Scripts.Item.Data
+{
+    [CreateAssetMenu(fileName = "ArmorItemData", menuName = "JJK/ArmorItemData", order = 0)]
+    public class ArmorItemData : ItemData
+    {
+        [field: SerializeField] public ArmorSO Armor { get; private set; }
+        [field: SerializeField] public ArmorType ArmorType { get; private set; }
+    }
+}

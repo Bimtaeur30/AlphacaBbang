a1d39ef5992ff64aaa5ebebac173b40275795668
ForRest1217 <Jihan2519@naver.com>
2026-05-19 17:48:02 +0900

    WolfEnemy

---
+++ AlphacaBbang/Assets/MemberWorkspace/JJH/02_Scripts/Agents/Enemies/BT/Conditions/CheckEnemyAttackRangeCloserThanCloseRangeToTargetCondition.cs
@@ -0,0 +1,26 @@
+using JJH._02_Scripts.Agents.Enemies;
+using System;
+using Unity.Behavior;
+using UnityEngine;
+
+[Serializable, Unity.Properties.GeneratePropertyBag]
+[Condition(name: "CheckEnemyAttackRangeCloserThanCloseRangeToTarget", story: "[Enemy] attackrange closer than [CloseRange] to [Target]", category: "Conditions", id: "a77bb287e3d3543d9f54de2aa65f9323")]
+public partial class CheckEnemyAttackRangeCloserThanCloseRangeToTargetCondition : Condition
+{
+    [SerializeReference] public BlackboardVariable<AbstractEnemy> Enemy;
+    [SerializeReference] public BlackboardVariable<float> CloseRange;
+    [SerializeReference] public BlackboardVariable<GameObject> Target;
+
+    public override bool IsTrue()
+    {
+        if (Enemy.Value == null || CloseRange.Value <= 0 || Target.Value == null)
+            return false;
+
+        float distance = Vector3.Distance(Enemy.Value.transform.position, Target.Value.transform.position);
+        float distanceToAttackRange = Mathf.Abs(distance - Enemy.Value.EnemyData.AttackRange);
+        float distanceToCloseRange = Mathf.Abs(distance - CloseRange.Value);
+        Debug.Log($"{distance} {distanceToAttackRange} {distanceToCloseRange}");
+
+        return distanceToAttackRange < distanceToCloseRange;
+    }
+}

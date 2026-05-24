f3b678b8ecbf34e5b23066507c9048d82f722ada
ForRest1217 <Jihan2519@naver.com>
2026-05-25 05:49:18 +0900

    PlayerGold

--- AlphacaBbang/Assets/MemberWorkspace/CHG/02_Scripts/PlayerStat/PlayerStatUp.cs
+++ AlphacaBbang/Assets/MemberWorkspace/CHG/02_Scripts/PlayerStat/PlayerStatUp.cs
@@ -1,9 +1,7 @@
-﻿using System;
+﻿using JJH._02_Scripts_Systems.EventSystems;
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Text;
-using DG.Tweening;
-using JJH._02_Scripts_Systems.EventSystems;
 using TMPro;
 using UnityEngine;
 
@@ -29,12 +27,12 @@ namespace MemberWorkspace.CHG._02_Scripts.PlayerStat
         [SerializeField] private EventChannelSO systemChannel;
         [SerializeField] private TextMeshProUGUI needItemText;
         [SerializeField] private PlayerStatStruct[] _statViews;
-        
+
         [SerializeField] private InventoryContainer inventory;
 
         private PlayerSaveData _playerSaveData;
         private SlidePanelController _slidePanelController;
-        
+
         private void Awake()
         {
             _slidePanelController = GetComponent<SlidePanelController>();
@@ -45,8 +43,8 @@ namespace MemberWorkspace.CHG._02_Scripts.PlayerStat
         {
             yield return null;
             yield return null;
-            
-            
+
+
             for (int i = 0; i < _statViews.Length; i++)
             {
                 _statViews[i].StatType = (PlayerStatType)i;
@@ -62,10 +60,10 @@ namespace MemberWorkspace.CHG._02_Scripts.PlayerStat
                 sb.AppendLine($"{StatLabels[idx]}: {pStruct.needItem.ItemName}X{pStruct.needItemCount}");
             }
             needItemText.text = sb.ToString();
-            
+
             foreach (PlayerStatStruct pStruct in _statViews)
             {
-                
+
                 int curStat = (int)GetCurrentStat(pStruct.StatType);
                 pStruct.statUpUi.StatTextChange(curStat.ToString(), (curStat + pStruct.statUpValue).ToString());
             }
@@ -73,9 +71,9 @@ namespace MemberWorkspace.CHG._02_Scripts.PlayerStat
 
         public void AddStat(int index)
         {
-            PlayerStatType type  = (PlayerStatType)index;
+            PlayerStatType type = (PlayerStatType)index;
             PlayerStatStruct pStruct = _statViews[index];
-            
+
             if (inventory.GetItemCount(pStruct.needItem) < pStruct.needItemCount)
             {
                 return;
@@ -105,26 +103,27 @@ namespace MemberWorkspace.CHG._02_Scripts.PlayerStat
 
             int updated = (int)GetCurrentStat(type);
             _statViews[idx].statUpUi.StatTextChange(updated.ToString(), (updated + _statViews[idx].statUpValue).ToString());
-            _statViews[idx].needItemCount +=  _statViews[idx].needValueIncrease;
-            
+            _statViews[idx].needItemCount += _statViews[idx].needValueIncrease;
+
             inventory.ConsumeBulletByName(pStruct.needItem.Id, pStruct.needItemCount);
         }
 
         private float GetCurrentStat(PlayerStatType type) => type switch
         {
-            PlayerStatType.Health     => _playerSaveData.MaxHealth,
-            PlayerStatType.Stamina    => _playerSaveData.MaxStamina,
+            PlayerStatType.Health => _playerSaveData.MaxHealth,
+            PlayerStatType.Stamina => _playerSaveData.MaxStamina,
             PlayerStatType.AimStamina => _playerSaveData.MaxAimStamina,
-            _                         => 0f
+            PlayerStatType.Gold => _playerSaveData.Gold,
+            _ => 0f
         };
 
         [ContextMenu("ddd")]
         private void ddd()
         {
-            transform.GetComponent<RectTransform>().position = _slidePanelController.HiddenPosition;            
-            
+            transform.GetComponent<RectTransform>().position = _slidePanelController.HiddenPosition;
+
         }
     }
-    
-    
+
+
 }

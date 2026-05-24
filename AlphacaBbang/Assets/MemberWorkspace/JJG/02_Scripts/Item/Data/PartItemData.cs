4a428232786d0ec8e66a9fcddbbf000262b12e3f
Valay0429 <a49986661@gmail.com>
2026-05-25 03:59:06 +0900

    CanEscape

--- AlphacaBbang/Assets/MemberWorkspace/JJG/02_Scripts/Item/Data/PartItemData.cs
+++ AlphacaBbang/Assets/MemberWorkspace/JJG/02_Scripts/Item/Data/PartItemData.cs
@@ -3,8 +3,9 @@
 namespace MemberWorkspace.JJG._02_Scripts.Item.Data
 {
     [CreateAssetMenu(fileName = "PartItemData", menuName = "JJK/PartItemData", order = 0)]
-    public class PartItemData : ScriptableObject
+    public class PartItemData : ItemData
     {
         [field:SerializeField] public int PartID { get; private set; }
+        [field:SerializeField] public Sprite Image { get; private set; }
     }
 }

2c2a4889fa24c7c6f9ede960613b697a207939d8
Bimtaeur30 <Bimtaeur30@gmail.com>
2026-05-25 02:15:05 +0900

    f

---
+++ AlphacaBbang/Assets/MemberWorkspace/KTJ/02_Scripts/UI/Esc.cs
@@ -0,0 +1,53 @@
+using DG.Tweening;
+using Reflex.Attributes;
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class Esc : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup CanvasGroup;
+    [SerializeField] private Button RestartBtn;
+    [SerializeField] private Button SettingBtn;
+    [SerializeField] private Button QuitBtn;
+
+    [Inject] SceneChangeManager sceneChangeManager;
+
+    bool isActive = false;
+    private void Start()
+    {
+        QuitBtn.onClick.AddListener(HandleQuitBtn);
+        SettingBtn.onClick.AddListener(HandleSettingBtn);
+        RestartBtn.onClick.AddListener(HandleRestartBtn);
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            isActive = true;
+            CanvasGroup.gameObject.SetActive(true);
+            CanvasGroup.DOFade(1f, 0.3f).SetUpdate(true);
+            Time.timeScale = 0f;
+        }
+    }
+
+    private void HandleQuitBtn()
+    {
+        Application.Quit();
+    }
+
+    private void HandleSettingBtn()
+    {
+        // ¼¼ÆÃ UI ¶ç¿ì±â
+    }
+
+    private void HandleRestartBtn()
+    {
+        isActive = false;
+        CanvasGroup.gameObject.SetActive(false);
+        CanvasGroup.DOFade(1f, 0.3f).SetUpdate(true);
+        Time.timeScale = 1f;
+    }
+}

cbd6090741b0db30ff119d24ec3ee74e0e67445c
ForRest1217 <Jihan2519@naver.com>
2026-05-25 15:26:19 +0900

    EnemyUI

--- AlphacaBbang/Assets/MemberWorkspace/JJH/02_Scripts/UIs/FollowWorldCanvas.cs
+++ AlphacaBbang/Assets/MemberWorkspace/JJH/02_Scripts/UIs/FollowWorldCanvas.cs
@@ -7,14 +7,20 @@ namespace JJH._02_Scripts.UIs
         [SerializeField] private Transform targetTrans;
         [SerializeField] private Vector3 offset;
 
+        private Camera cam;
         private Vector3 _velocity;
+
+        private void Start()
+        {
+            cam = Camera.main;
+        }
 
         private void LateUpdate()
         {
             if (targetTrans == null)
                 return;
 
-            Vector3 targetPos = targetTrans.position + offset;
+            Vector3 targetPos = targetTrans.position + (cam.transform.right * offset.x) + (cam.transform.up * offset.y);
             transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref _velocity, 0.05f);
         }
     }

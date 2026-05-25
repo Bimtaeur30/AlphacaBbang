cb0ecfdc1fd6bddaefaa16a2f8660cad58e371ae
ForRest1217 <Jihan2519@naver.com>
2026-05-25 12:43:14 +0900

    HitDamageUI

--- AlphacaBbang/Assets/MemberWorkspace/JJH/02_Scripts/Systems/EventSystems/AgentEvents.cs
+++ AlphacaBbang/Assets/MemberWorkspace/JJH/02_Scripts/Systems/EventSystems/AgentEvents.cs
@@ -29,11 +29,13 @@ namespace JJH._02_Scripts.Systems.EventSystems
 
     public class AgentHealthChangeEvent : GameEvent
     {
+        public Agent Agent { get; private set; }
         public float CurrentHealth { get; private set; }
         public float Damage { get; private set; }
 
-        public AgentHealthChangeEvent Init(float currentHealth, float damage)
+        public AgentHealthChangeEvent Init(Agent agent, float currentHealth, float damage)
         {
+            Agent = agent;
             Damage = damage;
             CurrentHealth = currentHealth;
             return this;

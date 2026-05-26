56ef1c6d6a632db5b43a8dcb0aebb614f24dee7b
ForRest1217 <Jihan2519@naver.com>
2026-05-26 21:12:31 +0900

    HealthVisible

--- AlphacaBbang/Assets/MemberWorkspace/CHG/02_Scripts/PlayerView/EnemyVisibility.cs
+++ AlphacaBbang/Assets/MemberWorkspace/CHG/02_Scripts/PlayerView/EnemyVisibility.cs
@@ -1,43 +1,49 @@
-﻿using System;
+﻿using JJH._02_Scripts.Agents.Enemies;
 using UnityEngine;
 
 namespace MemberWorkspace.CHG._02_Scripts.PlayerView
 {
     public class EnemyVisibility : MonoBehaviour, IVisible
     {
+        private AbstractEnemy enemy;
         private Renderer[] renderers;
 
         private void Awake()
         {
+            enemy = GetComponent<AbstractEnemy>();
             renderers = GetComponentsInChildren<Renderer>();
             Hide();
         }
 
         private void OnEnable()
         {
-            Hide(); 
+            Hide();
         }
 
         public void Show()
         {
-            if (renderers == null) return; 
-            
+            if (renderers == null) return;
+
             foreach (var r in renderers)
             {
-                if (r != null) 
+                if (r != null)
                     r.enabled = true;
             }
+
+            enemy.HealthModule.SetHealthBarVisible(true);
         }
 
         public void Hide()
         {
-            if (renderers == null) return; 
-            
+            if (renderers == null) return;
+
             foreach (var r in renderers)
             {
-                if (r != null) 
+                if (r != null)
                     r.enabled = false;
             }
+
+            enemy.HealthModule.SetHealthBarVisible(false);
         }
     }
 }
